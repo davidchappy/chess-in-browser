0488803a6a9eac523c453f04dbb31be488a86d0e
David Chapman <davidchappy@gmail.com>
2017-06-10 15:53:42 -0400

    Add request helpers to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,6 +41,9 @@ RSpec.configure do |config|
   # add `FactoryGirl` methods
   config.include FactoryGirl::Syntax::Methods
 
+  # add helper methods to request specs
+  config.include RequestSpecHelpers, type: :request
+
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)

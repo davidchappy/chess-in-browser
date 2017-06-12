50f3f3795ca359211a2617c565a439a3c88e8179
David Chapman <davidchappy@gmail.com>
2017-06-12 14:06:29 -0400

    Spec for piece generation on model

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -9,11 +9,34 @@ RSpec.describe Game, type: :model do
   it { should belong_to(:black) }
   it { should have_many(:pieces).dependent(:destroy) }
 
-  # validation tests
+  # Validation tests
   # ensure status is one of valid options
   it { should validate_inclusion_of(:status).in_array(["starting", "saved", "playing", "check", "check_mate"]) }
   # ensure player types are valid
   it { should validate_inclusion_of(:black_type).in_array(["Guest", "User"]) }
   it { should validate_inclusion_of(:white_type).in_array(["Guest", "User"]) }
+
+  describe '#generate' do
+    let(:guest1) { Guest.create(name: "Jim") }
+    let(:guest2) { Guest.create(name: "Bob") }
+    # let(:player1) { create(:player) }
+    # let(:player2) { create(:player) }
+    let!(:game) { Game.create(white: guest1, black: guest2, status: "starting") }
+
+    it "adds all game piece objects to a game object" do
+      expect(game.pieces).to_not be_empty
+      expect(game.pieces.length).to eq(32)
+      expect(game.white.pieces).to_not be_empty
+      expect(game.white.pieces.length).to eq(16)
+      expect(game.black.pieces).to_not be_empty
+      expect(game.black.pieces.length).to eq(16)
+    end
+
+    it "properly creates db records" do
+      expect(Game.last.pieces.length).to eq(32)
+      expect(Game.last.white.pieces.length).to eq(16)
+      expect(Game.last.black.pieces.length).to eq(16)
+    end
+  end
 
 end

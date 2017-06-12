3d666ff25b75dacf575eb2bc75b85957cf18addf
David Chapman <davidchappy@gmail.com>
2017-06-12 10:44:26 -0400

    Test DB Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612142140) do
+ActiveRecord::Schema.define(version: 20170612142738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20170612142140) do
   end
 
   create_table "pieces", force: :cascade do |t|
-    t.string "position"
+    t.string "position", default: "unplaced"
     t.string "type"
     t.bigint "player_id"
     t.bigint "game_id"

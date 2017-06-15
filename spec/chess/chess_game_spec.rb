97edfac357e1ce0625efbe5e178552e982aa89c9
David Chapman <davidchappy@gmail.com>
2017-06-14 21:45:49 -0400

    Add fill_board spec

--- spec/chess/chess_game_spec.rb
+++ spec/chess/chess_game_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require 'chess/chess'
 
 RSpec.describe Chess::Game do
-  let!(:valid_game) { create(:game) }
+  let(:valid_game) { create(:game) }
   let(:white) { valid_game.white }
   let(:black) { valid_game.black }
   let(:piece_classes) { ["Rook", "Knight", "Bishop", "King", "Queen", "Pawn"] }
@@ -24,6 +24,29 @@ RSpec.describe Chess::Game do
         expect(piece_classes).to include(piece.class.to_s)
       end
     end
+  end
+
+  describe '.fill_board' do
+    before do
+      Chess::Game.place_pieces(valid_game.white.pieces, :white).each(&:save!)
+      Chess::Game.place_pieces(valid_game.black.pieces, :black).each(&:save!)
+    end
+
+    it 'generates a hash chess board on game' do
+      board = Chess::Game.fill_board(valid_game).board
+      expect(board).to be_a(Hash)
+      expect(board.length).to eq(64)
+      expect(board.keys).to include("a1".to_sym)
+      expect(board.keys).to include("h8".to_sym)
+    end
+
+    it 'adds pieces to game board' do
+      board = Chess::Game.fill_board(valid_game).board
+      expect(board[:a8]).to be_a(Rook)
+      expect(board[:b2]).to be_a(Pawn)
+      expect(board[:g8]).to be_a(Knight)
+    end
+
   end
 
 end

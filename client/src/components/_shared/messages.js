40f0c7b72d6b465712769d699663bb503887ce4e
David Chapman <davidchappy@gmail.com>
2017-07-03 11:43:20 -0400

    Messages in client show check mate also

--- client/src/components/_shared/messages.js
+++ client/src/components/_shared/messages.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { currentPlayer } from '../../utils/helper';
+import { currentPlayer, otherPlayer } from '../../utils/helper';
 
 const Messages = (props) => {
 
-  const playerName = currentPlayer(props.white, props.black).name;
+  const player = currentPlayer(props.white, props.black).name;
+  const other = otherPlayer(props.white, props.black).name;
 
   const message = () => {
     if(props.game.status === 'check') {
       return <span>  |  Check!</span>;
+    } else if(props.game.status === 'check_mate') {
+      return <span>  |  Check Mate! {other} wins!</span> 
     } else {
       return '';
     }
@@ -16,7 +19,7 @@ const Messages = (props) => {
   return (
 
     <div className="Messages">
-      <p><span>Now Playing: {playerName}</span>{message()}</p>
+      <p><span>Now Playing: {player}</span>{message()}</p>
     </div>
 
   )
--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -1,3 +1,7 @@
 export function currentPlayer(white, black) {
   return white.is_playing ? white : black;
 }
+
+export function otherPlayer(white, black) {
+  return white.is_playing ? black : white;
+}
